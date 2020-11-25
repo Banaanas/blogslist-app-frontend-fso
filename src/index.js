43d21d4664eb7f4dbebd2e6e42b43b8319ec6edf
Cyril <cyrildeveloppeur@gmail.com>
2020-11-25 08:56:33 +0100

    style:

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "./store/store";
 import AppWrapper from "./AppWrapper";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,8 +18,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root"),
 );
-
+/*
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();*/
