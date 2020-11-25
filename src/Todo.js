ae32357334bf0d6b8f7e8357d020b3e221dbb13f
Cyril <cyrildeveloppeur@gmail.com>
2020-11-25 08:46:59 +0100

    style:

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,10 @@
 // Todo - Tests
 // Todo - Demo Identifiers
 // Todo - Fake Data (blogs, authors, etc.)
-// Todo - Front-End Tests
 // Todo - Refactoring
 // Todo - Deploy
 // Todo - Change password for backend DBs
 // Todo - Verify passwords and .gitignore
-// Todo - Delete Components Files not used
 // Todo - ReadMe
 
 // Todo - AirBnb Config in .eslintrc.json has been disabled
